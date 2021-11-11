4bc2f1fd48e448453cc79b4dc624470037db8e18
William Schramm <william.schramm11@gmail.com>
2021-11-11 18:23:41 -0500

    QuoteBox now passes tests.

--- src/components/QuoteBox.js
+++ src/components/QuoteBox.js
@@ -1,6 +1,10 @@
 import {useState, useEffect} from 'react'
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
 
 const QuoteBox = props => {
     const [quote, setQuote] = useState("")
@@ -22,7 +26,16 @@ const QuoteBox = props => {
             <Card.Body>
                 <Card.Title id="text" style={{color: 'black'}}>{quote.content}</Card.Title>
                 <Card.Text id="author" style={{color: 'black'}}>{quote.author}</Card.Text>
-                <Button variant="primary">Go somewhere</Button>
+                <Container>
+                    <Row> 
+                        <Col md={2}> 
+                            <Button id="tweet-quote" variant="primary" href="https://twitter.com/intent/tweet">Tweet</Button>
+                        </Col>
+                        <Col md={{span: 4, offset: 6}}>
+                            <Button id="new-quote" variant="primary" onClick={getRandomQuote}>New Quote</Button>
+                        </Col>                        
+                    </Row>  
+                </Container>
             </Card.Body>
         </Card>
     )
